2d3074442c924e88dd9d36db33b690917e637b59
Sajid Ishaq <SAJIDISHAK7@GMAIL.COM>
2021-10-11 01:38:26 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from "react";
+import CardItem from './components/Card';
+import SirInzamam from "./Images/Sir_Inzamam_Profile.jpg";
+import Burger from "./Images/burger.jpg";
+import Saqib from "./Images/saqib.jpeg";
+import Shop from "./Images/victory.jpeg";
+import Sufiyan from "./Images/sufiyan.jpg";
+import Express from "./Images/express.jpg";
+import Ishaq from "./Images/ishaq.jpeg";
+import Tour from "./Images/tour.jpeg";
+import Sajid from "./Images/sajid.jpeg";
+import Villa from "./Images/villa.jpeg";
+import Shafiq from "./Images/shafiq.jpeg";
+import Babies from "./Images/babies.jpeg";
+import Haroon from "./Images/haroon.jpeg";
+import HiRoof from "./Images/hiroof.jpg";
+import Zain from "./Images/zain.jpeg";
+import FisalMasjid from "./Images/fisalmasjid.jpeg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      marginTop: "40px",
+      justifyContent: "space-around",
+      flexWrap: "wrap",
+      margin: "10px"
+    }}>
+      <CardItem profile={SirInzamam} title="Inzamam Malik" date="04 Oct 2021" postimage={Burger} dis="Very Delicious Burger Thanks for treat @Fahim_Uz_Zaman"/>
+      <CardItem profile={Saqib} title="Saqib Ishaq" date="26 Mar 2021" postimage={Shop} dis="I've redesigned my shop, so tell me guys how it is?"/>
+      <CardItem profile={Sufiyan} title="Inno Sufiyan" date="28 Sep 2021" postimage={Express} dis="Yuhuu: I purchase new express 70CC bike"/>
+      <CardItem profile={Ishaq} title="Ishaq Sharif" date="28 Aug 2019" postimage={Tour} dis="Me & my my friend visited river of Chanab, After a long time & it was great place."/>
+      <CardItem profile={Sajid} title="Sajid Ishaq" date="01 Sep 2021" postimage={Villa} dis="Lovely Weather in Bahria Town Karachi"/>
+      <CardItem profile={Shafiq} title="Shafiq Ahmed" date="14 Aug 2021" postimage={Babies} dis="May Allah Almighty keep you happy, my dear daughters. Happy Independence Day"/>
+      <CardItem profile={Haroon} title="Rana Haroon" date="10 Oct 2021" postimage={HiRoof} dis="By the grace of Allah Almighty, I took the Hi-Roof"/>
+      <CardItem profile={Zain} title="Zain Ul Abideen" date="24 Aug 2021" postimage={FisalMasjid} dis="I visited Faisal Masjid. This the very biggest masjid of Pakistan."/>
     </div>
-  );
+  )
 }
 
 export default App;
